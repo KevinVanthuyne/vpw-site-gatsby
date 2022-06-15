3c55d947c5b08797da5d139e2b99b54b649c98a6
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-15 20:44:32 +0200

    Added an example StaticImage

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,21 @@
 import * as React from 'react'
 import {Link} from 'gatsby'
 import {navContainer, navItem} from './layout.module.css'
+import {StaticImage} from 'gatsby-plugin-image'
 
 const Layout = ({pageTitle, children}) => {
     return (
         <div>
             <title>{pageTitle} | VPW</title>
-            <nav>
-                <ul className={navContainer}>
-                    <li className={navItem}><Link to="/">Home</Link></li>
-                    <li className={navItem}><Link to="/about">About</Link></li>
-                </ul>
-            </nav>
+            <div>
+                <StaticImage src="../images/vpw-icon-solo.png" alt="VPW icon"/>
+                <nav>
+                    <ul className={navContainer}>
+                        <li className={navItem}><Link to="/">Home</Link></li>
+                        <li className={navItem}><Link to="/about">About</Link></li>
+                    </ul>
+                </nav>
+            </div>
             <main>
                 <h1>{pageTitle}</h1>
                 {children}
