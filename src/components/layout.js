e2e3b6ece552c2f0d1ae2f5e2c4bdc019997b69d
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-15 20:50:16 +0200

    Added a static query for the site title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,23 @@
 import * as React from 'react'
-import {Link} from 'gatsby'
+import {graphql, Link, useStaticQuery} from 'gatsby'
 import {navContainer, navItem} from './layout.module.css'
 import {StaticImage} from 'gatsby-plugin-image'
 
 const Layout = ({pageTitle, children}) => {
+    const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
     return (
         <div>
-            <title>{pageTitle} | VPW</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header>{data.site.siteMetadata.title}</header>
             <div>
                 <StaticImage src="../images/vpw-icon-solo.png" alt="VPW icon"/>
                 <nav>
