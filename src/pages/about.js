30fcd7a71eacf9576d272df8ba7abafb1161821e
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-15 20:23:53 +0200

    Added about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About</title>
+            <h1>About</h1>
+            <p>We are the Virtual Pinball Workshop team.</p>
+        </main>
+    )
+}
+
+export default AboutPage
