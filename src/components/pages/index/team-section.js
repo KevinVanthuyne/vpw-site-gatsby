ec850e0a24679494a80f3d8701aab3bd2e4347e6
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-20 15:17:25 +0200

    Added team section

---
+++ src/components/pages/index/team-section.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const TeamSection = () => (
+    <div className="row mt-10">
+        <div className="col-md-5">
+            <div id="globe-svg" className="h-50"></div>
+        </div>
+
+        <div className="col-md-4 vh-100 offset-md-1 col-lg-2">
+            <h2 className="big-title mt-10">A Dedicated Team</h2>
+            <p className="display-text">
+                Consisting out of more than 100 members from around the globe,
+                each member brings his own skills and talents.
+            </p>
+        </div>
+    </div>
+);
+
+export default TeamSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Layout from '../components/layout';
 
 import LandingSection from '../components/pages/index/landing-section';
 import MissionSection from '../components/pages/index/mission-section';
+import TeamSection from '../components/pages/index/team-section';
 
 const IndexPage = () => (
     <Layout pageTitle="Home">
         <LandingSection />
         <MissionSection />
+        <TeamSection />
     </Layout>
 );
 
