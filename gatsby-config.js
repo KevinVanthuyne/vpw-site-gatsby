a057d21e5b57971cd29ee049b81b34c72fffe6e3
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-20 12:53:36 +0200

    Added webfonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,22 @@
 module.exports = {
-  siteMetadata: {
-    title: `VPW Site`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `VPW Site`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "images"
-  }]
+    plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            "name": "images",
+            "path": "./src/images/"
+        },
+        __key: "images"
+    },
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Karla', 'Anton']
+                }
+            }
+        }]
 };
