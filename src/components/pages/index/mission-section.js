4141e673126889cab3662039d87f790c500cffbd
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-21 12:02:55 +0200

    Added scroll trigger animation for BM image

--- src/components/pages/index/mission-section.js
+++ src/components/pages/index/mission-section.js
@@ -1,28 +1,66 @@
 import * as React from 'react';
+import { useEffect, useRef } from 'react';
 
 import { StaticImage } from 'gatsby-plugin-image';
 import DisplayButton from '../../display-button';
+import { Circ, gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-const MissionSection = () => (
-    <div className="row vh-100">
-        <div className="col-md-4 offset-md-1 col-lg-2 offset-lg-2">
-            <h2 className="big-title mt-10">The Future of Virtual Pinball</h2>
-            <p className="display-text">
-                We strive towards <span className="beige-text">re</span>creating
-                the best virtual versions of pinball tables that Virtual Pinball
-                can offer.
-            </p>
-            <DisplayButton link="/">Our mission →</DisplayButton>
-        </div>
+const MissionSection = () => {
+    gsap.registerPlugin(ScrollTrigger);
 
-        <div className="col-md-6 col-lg-5 col-xl-5 offset-xl-1 col-xxl-4">
-            <StaticImage
-                src="../../../images/bm_side_view.png"
-                alt="Blood Machines pinball cabinet render"
-                layout="fullWidth"
-            />
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const element = ref.current;
+        gsap.fromTo(
+            element.querySelector('.bm-image'),
+            {
+                opacity: 0,
+                xPercent: 100,
+            },
+            {
+                opacity: 1,
+                xPercent: 0,
+                ease: Circ.easeOut,
+                scrollTrigger: {
+                    trigger: element.querySelector('.mission-row'),
+                    start: 'top bottom',
+                    end: 'top 25%',
+                    scrub: true,
+                    markers: true,
+                },
+            }
+        );
+    }, []);
+
+    return (
+        <div ref={ref}>
+            <div className="row vh-100 mission-row">
+                <div className="col-md-4 offset-md-1 col-lg-2 offset-lg-2">
+                    <h2 className="big-title mt-10">
+                        The Future of Virtual Pinball
+                    </h2>
+                    <p className="display-text">
+                        We strive towards <span className="beige-text">re</span>
+                        creating the best virtual versions of pinball tables
+                        that Virtual Pinball can offer.
+                    </p>
+                    <DisplayButton link="/">Our mission →</DisplayButton>
+                </div>
+
+                <div className="col-md-6 col-lg-5 col-xl-5 offset-xl-1 col-xxl-4">
+                    <StaticImage
+                        src="../../../images/bm_side_view.png"
+                        alt="Blood Machines pinball cabinet render"
+                        layout="fullWidth"
+                        className="bm-image"
+                    />
+                </div>
+            </div>
+            <div className="mission-row-end" />
         </div>
-    </div>
-);
+    );
+};
 
 export default MissionSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
 import Layout from '../components/layout';
 import LandingSection from '../components/pages/index/landing-section';
 import MissionSection from '../components/pages/index/mission-section';
@@ -8,19 +6,8 @@ import TeamSection from '../components/pages/index/team-section';
 import WorkSection from '../components/pages/index/work-section';
 
 const IndexPage = () => {
-    // store a reference to the box div
-    const boxRef = useRef();
-
-    // wait until DOM has been rendered
-    useEffect(() => {
-        gsap.to(boxRef.current, { rotation: '+=360' });
-    });
-
     return (
         <Layout pageTitle="Home">
-            <div className="box" ref={boxRef}>
-                Hello
-            </div>
             <LandingSection />
             <MissionSection />
             <TeamSection />
