cb375abb60640ea94d29bb39e24513c44b3baf73
Kevin Vanthuyne <kevin.vanthuyne@comsof.com>
2022-06-22 15:47:17 +0200

    Made pinball animation in landing screen logo

--- src/components/pages/index/landing-section.js
+++ src/components/pages/index/landing-section.js
@@ -3,8 +3,11 @@ import { useEffect, useRef } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import * as styles from './landing-section.module.scss';
 import { Circ, gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const LandingSection = () => {
+    gsap.registerPlugin(ScrollTrigger);
+
     const ref = useRef(null);
 
     useEffect(() => {
@@ -22,16 +25,55 @@ const LandingSection = () => {
                 duration: 1.5,
             }
         );
+        gsap.fromTo(
+            element.querySelector('.vpw-logo-2'),
+            {
+                yPercent: 200,
+                opacity: 0,
+            },
+            {
+                yPercent: 0,
+                opacity: 1,
+                ease: Circ.easeOut,
+                duration: 1.5,
+            }
+        );
+        gsap.fromTo(
+            element.querySelector('.vpw-logo-2'),
+            {
+                yPercent: 0,
+            },
+            {
+                yPercent: -200,
+                scrollTrigger: {
+                    trigger: element.querySelector('.landing-row'),
+                    start: 'bottom bottom',
+                    end: 'bottom 25%',
+                    scrub: true,
+                    // markers: true,
+                },
+            }
+        );
     }, []);
 
     return (
         <div className={'row ' + styles.backgroundOverlay}>
             <div className={'vh-120 ' + styles.background} ref={ref}>
-                <div className="offset-sm-1 col-sm-10 offset-md-2 col-md-8 offset-lg-3 col-lg-6 vh-100 d-flex justify-content-center align-items-center">
+                <div
+                    className={
+                        styles.imageContainer + ' landing-row'
+                        // ' offset-sm-1 col-sm-10 offset-md-2 col-md-8 offset-lg-3 col-lg-6'
+                    }
+                >
                     <StaticImage
-                        src="../../../images/vpw-logo.png"
-                        alt="VPW logo"
-                        className="img-fluid vpw-logo"
+                        src="../../../images/vpw-logo-without-pinball.png"
+                        alt="VPW logo without the pinball"
+                        className={styles.centerImage + ' img-fluid vpw-logo'}
+                    />
+                    <StaticImage
+                        src="../../../images/vpw-logo-pinball.png"
+                        alt="Globe and pinball combination of the VPW logo"
+                        className={styles.centerImage + ' img-fluid vpw-logo-2'}
                     />
                 </div>
             </div>
