ff56af02e3df2e0037c350fcef48903395b699d7
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-08-05 20:06:21 +0200

    Improved footer responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,10 @@ import * as styles from './footer.module.scss';
 const Footer = () => (
     <footer className={styles.footer + ' container-fluid'}>
         <div className="row p-5">
-            <div className="col-md-3">&copy; 2022 V-Pin Workshop</div>
-            <div className="offset-md-6 col-md-3 text-end">
+            <div className="col-md-3 text-center text-md-start">
+                &copy; 2022 V-Pin Workshop
+            </div>
+            <div className="offset-md-6 col-md-3 text-center text-md-end">
                 By{' '}
                 <a target="_blank" rel="noopener" href="https://kevinvt.com">
                     kevinvt.com
