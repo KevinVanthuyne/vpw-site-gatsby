b92e259ca9b78b5260e2c339cd596d9a08b33aa7
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-08-06 16:43:58 +0200

    First iteration of dynamically fetching table images for work page

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
+import TableItem from '../components/table-item';
 
-const WorkPage = () => (
+const WorkPage = ({ data }) => (
     <Layout pageTitle="Work">
         <div className="row mt-10 mb-5">
             <div className="col-md-3 offset-md-1">
@@ -13,7 +15,7 @@ const WorkPage = () => (
                     <a
                         href="https://vpuniverse.com/profile/40692-vpinworkshop/"
                         target="_blank"
-                        rel="noopener"
+                        rel="noreferrer"
                     >
                         VPW profile on VPUniverse
                     </a>
@@ -21,17 +23,35 @@ const WorkPage = () => (
                 </p>
             </div>
 
-            {/*<div className="col-md-2 offset-md-1">*/}
-            {/*    <img src="/assets/img/tables/tftc.jpg" alt="Tales From The Crypt screenshot" className="img-fluid">*/}
-            {/*</div>*/}
-
-            {/*<div className="col-md-2 offset-md-1">*/}
-            {/*    <a href="https://vpuniverse.com/files/file/10193-the-whos-tommy-pinball-wizard-data-east-1994-vpwmod/">*/}
-            {/*        <img src="/assets/img/tables/tommy.png" alt="Tommy screenshot" className="img-fluid">*/}
-            {/*    </a>*/}
-            {/*</div>*/}
+            {data.allFile.nodes.map((node) => (
+                <div className="col-md-2 offset-md-1">
+                    <TableItem
+                        link="https://vpuniverse.com/files/file/6933-tales-from-the-crypt-vpw-premium-data-east-1993/"
+                        alt="Tales From the Crypt screenshot"
+                        img={node.childImageSharp.gatsbyImageData}
+                    />
+                </div>
+            ))}
         </div>
     </Layout>
 );
+
+export const query = graphql`
+    query {
+        allFile(
+            filter: {
+                sourceInstanceName: { eq: "images" }
+                relativeDirectory: { eq: "tables" }
+            }
+        ) {
+            nodes {
+                name
+                childImageSharp {
+                    gatsbyImageData
+                }
+            }
+        }
+    }
+`;
 
 export default WorkPage;
