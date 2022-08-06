c72dc0a61fa79e814d9102e8368c54343798916b
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-08-06 11:54:58 +0200

    Formatted gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,29 @@
 module.exports = {
     siteMetadata: {
         title: `VPW Site`,
-        siteUrl: `https://www.yourdomain.tld`
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            "name": "images",
-            "path": "./src/images/"
+    plugins: [
+        'gatsby-plugin-image',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: './src/images/',
+            },
+            __key: 'images',
         },
-        __key: "images"
-    },
         {
             resolve: 'gatsby-plugin-web-font-loader',
             options: {
                 google: {
-                    families: ['Karla', 'Anton']
-                }
-            }
-        }]
+                    families: ['Karla', 'Anton'],
+                },
+            },
+        },
+    ],
 };
