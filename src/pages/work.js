4b5a42b3acca48e02f422fbe07a280fb79a705bd
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-08-06 18:15:39 +0200

    Implemented Swiper - beautiful!

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,8 +1,16 @@
 import * as React from 'react';
 import Layout from '../components/layout';
-import { GatsbyImage, StaticImage } from 'gatsby-plugin-image';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const WorkPage = ({ data }) => (
+import 'swiper/scss';
+import 'swiper/scss/effect-coverflow';
+import 'swiper/scss/pagination';
+
+import { EffectCoverflow, Pagination, Mousewheel, Keyboard } from 'swiper';
+import MediaQueryUtil from '../util/media-query-util';
+
+const WorkPage = () => (
     <Layout pageTitle="Work">
         <div className="row mt-10 mb-5">
             <div className="col-md-3 offset-md-1">
@@ -21,19 +29,86 @@ const WorkPage = ({ data }) => (
                     .
                 </p>
             </div>
-
-            <div className="col-md-2 offset-md-1">
-                <a
-                    href="https://vpuniverse.com/files/file/6933-tales-from-the-crypt-vpw-premium-data-east-1993/"
-                    target="_blank"
-                    rel="noreferrer"
+        </div>
+        <div className="row mb-5">
+            <div className="col gx-0">
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    autoHeight={true}
+                    coverflowEffect={{
+                        rotate: 30,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={true}
+                    modules={[
+                        EffectCoverflow,
+                        Pagination,
+                        Keyboard,
+                        Mousewheel,
+                    ]}
+                    slidesPerView={1.3}
+                    spaceBetween={15}
+                    loop={true}
+                    breakpoints={{
+                        [MediaQueryUtil.MD_BREAKPOINT]: {
+                            slidesPerView: 2,
+                            spaceBetween: 25,
+                        },
+                        [MediaQueryUtil.LG_BREAKPOINT]: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                        },
+                        [MediaQueryUtil.XL_BREAKPOINT]: {
+                            slidesPerView: 4,
+                            spaceBetween: 40,
+                        },
+                    }}
                 >
-                    <StaticImage
-                        src="../images/tables/tftc.jpg"
-                        alt="Top-down view of Tales From The Crypt"
-                        layout="fullWidth"
-                    />
-                </a>
+                    <SwiperSlide>
+                        <a
+                            href="https://vpuniverse.com/files/file/6933-tales-from-the-crypt-vpw-premium-data-east-1993/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <StaticImage
+                                src="../images/tables/tftc.jpg"
+                                alt="Top-down view of Tales From The Crypt"
+                                layout="fullWidth"
+                            />
+                        </a>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <a
+                            href="https://vpuniverse.com/files/file/9847-blood-machines-vpw-original-2022/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <StaticImage
+                                src="../images/tables/bm.jpg"
+                                alt="Top-down view of Blood Machines"
+                                layout="fullWidth"
+                            />
+                        </a>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <a
+                            href="https://vpuniverse.com/files/file/8828-lord-of-the-rings-valinor-edition-stern-2003-vpw/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <StaticImage
+                                src="../images/tables/lotr.jpg"
+                                alt="Top-down view of Lord of the Rings"
+                                layout="fullWidth"
+                            />
+                        </a>
+                    </SwiperSlide>
+                </Swiper>
             </div>
         </div>
     </Layout>
--- src/util/media-query-util.js
+++ src/util/media-query-util.js
@@ -1,10 +1,16 @@
 class MediaQueryUtil {
+    static SM_BREAKPOINT = 576;
+    static MD_BREAKPOINT = 768;
+    static LG_BREAKPOINT = 992;
+    static XL_BREAKPOINT = 1200;
+    static XXL_BREAKPOINT = 1400;
+
     /**
      * @returns {boolean} If the screen size is smaller than Bootstrap's medium breakpoint
      */
     static isMobile() {
-        return window.matchMedia("only screen and (max-width: 768px)").matches;
+        return window.matchMedia('only screen and (max-width: 768px)').matches;
     }
 }
 
-export default MediaQueryUtil
+export default MediaQueryUtil;
