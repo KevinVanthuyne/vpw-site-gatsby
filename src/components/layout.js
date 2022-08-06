5e03da23e69cdaa8653342de98beb7e2898bd04d
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-08-06 12:03:03 +0200

    Fixed footer not at bottom of pages with little content

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,11 @@ import Header from './header';
 import Footer from './footer';
 
 const Layout = ({ pageTitle, children }) => (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
         <Header />
 
-        <div className="container-fluid overflow-hidden">
-            <main>
+        <div className="container-fluid overflow-hidden flex-grow-1">
+            <main className="">
                 <h1 className="d-none">{pageTitle}</h1>
                 {children}
             </main>
