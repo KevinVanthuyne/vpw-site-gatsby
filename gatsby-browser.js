a5f825df2cccb58c9ea77ffc91618a709cab332e
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2022-08-07 15:08:18 +0200

    Fixed navigating pages not starting at the top

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,13 @@
 import './src/styles/global.scss';
+
+// Always start at the top of the page on a route change.
+
+export const onRouteUpdate = () => {
+    if (typeof window !== `undefined`) {
+        window.scrollTo(0, 0);
+    }
+};
+
+export const shouldUpdateScroll = (args) => {
+    return false;
+};
